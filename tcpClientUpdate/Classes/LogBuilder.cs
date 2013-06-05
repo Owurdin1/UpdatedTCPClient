775f0e379fbc5e777df2565e94461555d47f00b4
Oscar Wurdinger <owurdin1@gmail.com>
2013-06-05 13:13:28 -0700

    Seems to be a lot more reliable. My server has to run at 7-10ms for 10k msgs.

--- tcpClientUpdate/Classes/LogBuilder.cs
+++ tcpClientUpdate/Classes/LogBuilder.cs
@@ -8,6 +8,10 @@ namespace tcpClientUpdate.Classes
 {
     class LogBuilder
     {
+        // private contant global variables
+        private const int RECEIVE_FLAG = 0;
+        private const int SEND_FLAG = 1;
+
         public List<byte[]> receivedMessages = new List<byte[]>();
         public List<byte[]> sentMessages = new List<byte[]>();
 
@@ -35,15 +39,65 @@ namespace tcpClientUpdate.Classes
                     sw.Write(recStr + "\n\r");
                 }
 
-                // Trailer Record:
-                // MMDDYYYY | HHMMSS | 0 | 0 | 0
-                StringBuilder trailerSB = new StringBuilder();
-                trailerSB.Append(System.DateTime.Now.ToString("MMddyyyy"));
-                trailerSB.Append("|");
-                trailerSB.Append(System.DateTime.Now.ToString("HHmmss"));
-                trailerSB.Append("|0|0|0");
-                sw.Write(trailerSB.ToString());
             }
+            else
+            {
+                int count = receivedMessages.Count;
+                int flag = RECEIVE_FLAG;
+                if (count > sentMessages.Count)
+                {
+                    count = sentMessages.Count;
+                    flag = SEND_FLAG;
+                }
+
+                for (int i = 0; i < count; i++)
+                {
+                    string recStr = System.Text.Encoding.ASCII.GetString(receivedMessages[i]);
+                    byte[] choppedRec = new byte[sentMessages[i].Length - 2];
+                    Array.Copy(sentMessages[i], 2, choppedRec, 0, choppedRec.Length);
+
+                    string sentStr = System.Text.Encoding.ASCII.GetString(choppedRec);
+
+                    //string sentStr = System.Text.Encoding.ASCII.GetString(sentMessages[i]);
+
+                    sw.Write(sentStr + "\n\r");
+                    sw.Write(recStr + "\n\r");
+
+                }
+
+                switch (flag)
+                {
+                    case RECEIVE_FLAG:
+                        for (int i = count; i < receivedMessages.Count; i++)
+                        {
+                            string recStr = System.Text.Encoding.ASCII.GetString(receivedMessages[i]);
+                            byte[] choppedRec = new byte[receivedMessages[i].Length - 2];
+                            Array.Copy(receivedMessages[i], 2, choppedRec, 0, choppedRec.Length);
+                            sw.Write(recStr + "\n\r");
+                        }
+                        break;
+
+                    case SEND_FLAG:
+                        for (int i = count; i < sentMessages.Count; i++)
+                        {
+                            string sentStr = System.Text.Encoding.ASCII.GetString(sentMessages[i]);
+                            byte[] choppedRec = new byte[sentMessages[i].Length - 2];
+                            Array.Copy(sentMessages[i], 2, choppedRec, 0, choppedRec.Length);
+                            sw.Write(sentStr + "\n\r");
+                        }
+                        break;
+                }
+
+            }
+
+            // Trailer Record:
+            // MMDDYYYY | HHMMSS | 0 | 0 | 0
+            StringBuilder trailerSB = new StringBuilder();
+            trailerSB.Append(System.DateTime.Now.ToString("MMddyyyy"));
+            trailerSB.Append("|");
+            trailerSB.Append(System.DateTime.Now.ToString("HHmmss"));
+            trailerSB.Append("|0|0|0");
+            sw.Write(trailerSB.ToString());
 
             sw.Close();
             System.Windows.Forms.MessageBox.Show("Finished logging Received Count: " + receivedMessages.Count.ToString() + " Sent Count: " + sentMessages.Count.ToString());
